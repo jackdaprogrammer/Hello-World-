ae47d0eafdc5dc04791ee7bc3edc4618f417effc
jackdaprogrammer <32852582+jackdaprogrammer@users.noreply.github.com>
2018-02-01 13:40:49 -0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+//Jack Hosier, Eclipse Hello World Project
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+    //Do I get an 'A' for effort?
+	}
+
+}
